ebee3376faa0727316ea0196a9faa57c8e110420
Jimgeo98 <geojimas@gmail.com>
2021-04-21 01:41:12 +0300

    code organised

--- src/App.js
+++ src/App.js
@@ -1,71 +1,64 @@
-import React, { useEffect, useState } from 'react';
-import Movie from './components/Movie';
+import React, { useEffect, useState } from "react";
+import Movie from "./components/Movie";
 
-const API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 
-const SEARCH = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
-
+const SEARCH =
+  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [item, setItem] = useState("");
 
-    const [ movies, setMovies ] = useState([])
-    const [item, setItem ] = useState('')
+  useEffect(() => {
+    getMovies(API);
+  }, []);
 
-    useEffect( () => {
-        getMovies(API)
-    }, [])
+  const getMovies = (api) => {
+    fetch(api)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+  };
 
+  const handlerOnSubmit = (e) => {
+    e.preventDefault();
 
-    const getMovies = (api) =>  {
-        fetch(api)
-            .then( (res) => res.json())
-            .then((data) => {
-                setMovies(data.results)
-            })
+    if (item) {
+      getMovies(SEARCH + item);
+      setItem("");
     }
+  };
 
+  const handleOnChange = (e) => {
+    setItem(e.target.value);
+  };
 
-    const handlerOnSubmit = (e) => {
-        e.preventDefault();
+  return (
+    <div>
+      <header>
+        <form onSubmit={handlerOnSubmit}>
+          <input
+            type="search"
+            className="search"
+            value={item}
+            onChange={handleOnChange}
+            placeholder="Search..."
+          />
+          <button type="submit" className="btn">
+            <i className="fas fa-search"></i>
+          </button>
+        </form>
+      </header>
 
-        if (item) {
-            getMovies(SEARCH + item)
-            setItem('')
-        }
-    }
-
-
-    const handleOnChange = (e) => {
-        setItem(e.target.value)
-    }
-
-    
-    return (
-        <div>
-            <header>
-                <form onSubmit={handlerOnSubmit}>
-                    <input
-                        type="search" 
-                        className="search" 
-                        value={item} 
-                        onChange={handleOnChange} 
-                        placeholder="Search..." 
-                     />
-                    <button 
-                        type="submit" 
-                        className="btn">
-                        <i className="fas fa-search"></i>
-                    </button>
-                </form>
-            </header>
-
-            <div className="container">
-                {movies.length > 0 && movies.map( (i) => 
-                    <Movie key={movies.id} {...i} />
-                )}
-            </div>
-        </div>
-    )
+      <div className="container">
+        {movies.length > 0 &&
+          movies.map((i) => <Movie key={movies.id} {...i} />)}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,48 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const IMG = "https://image.tmdb.org/t/p/w1280";
 
-
 const setVoteClass = (vote) => {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+};
 
-    if (vote >= 8) {
-        return "green"
-    } else if (vote >= 5 ) {
-        return "orange"
-    }
-    else {
-        return "red"
-    }
-}
+const Movie = ({ title, poster_path, overview, vote_average }) => {
+  return (
+    <div className="movie">
+      <img
+        src={
+          poster_path
+            ? IMG + poster_path
+            : "https://images.unsplash.com/photo-1535016120720-40c646be5580?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+        }
+        alt={title}
+      />
 
+      <div className="info">
+        <h3>{title}</h3>
+        <h4 className={`tag ${setVoteClass(vote_average)}`}>
+          {vote_average}
+          <i className="fas fa-medal"></i>
+        </h4>
+      </div>
 
+      <div className="overview">
+        <h2>
+          <i className="far fa-file-alt"></i> Overview:
+        </h2>
+        <p>{overview}</p>
+      </div>
+    </div>
+  );
+};
 
-
-const Movie = ({title, poster_path, overview, vote_average}) => {
-    return(
-        <div className="movie">
-            <img 
-                src={ poster_path ? ( IMG + poster_path) :
-                        'https://images.unsplash.com/photo-1535016120720-40c646be5580?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
-                    } 
-                    alt={title} 
-                />
-
-            <div className="info">
-                <h3>{title}</h3>
-                <h4 
-                    className={ `tag ${setVoteClass(vote_average)}`}>
-                    {vote_average} 
-                    <i className="fas fa-medal"></i>
-                </h4>
-            </div>
-
-            <div className="overview">
-                <h2><i className="far fa-file-alt"></i> Overview:</h2>
-                <p>{overview}</p>
-            </div>
-        </div>
-    )
-}
-
-export default Movie
+export default Movie;
